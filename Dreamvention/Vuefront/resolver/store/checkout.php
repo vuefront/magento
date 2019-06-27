393e411269294f3b7ec032b3ae70e7332327870c
Alexandrvn <php4.drvn@gmail.com>
2019-06-27 11:08:07 +0300

    Fix api for checkout

--- Dreamvention/Vuefront/resolver/store/checkout.php
+++ Dreamvention/Vuefront/resolver/store/checkout.php
@@ -1,10 +1,15 @@
 <?php
+use \Magento\Framework\App\ObjectManager;
 
 class ResolverStoreCheckout extends Resolver
 {
     public function link() {
+        $objectManager =ObjectManager::getInstance();
+
+        $url = $objectManager->get('\Magento\Framework\UrlInterface');
+
         return array(
-            'link' => WC()->cart->get_checkout_url()
+            'link' => $url->getUrl('checkout')
         );
     }
 }
